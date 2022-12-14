4c485019f36325bccb61228ec3b599373629bf96
phibey06 <116543645+phibey06@users.noreply.github.com>
2022-12-14 09:34:10 +0100

    Add files via upload

---
+++ login/mittscript.js
@@ -0,0 +1,7 @@
+function minfunksjon() {
+    var brukernavn = document.getElementById("brukernavn").value;
+
+    document.getElementById("output").innerHTML = "Velkommen inn, "+brukernavn+"!";
+        console.log("Hei "+brukernavn+"")
+
+}
